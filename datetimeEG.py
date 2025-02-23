085430e7c97ca5d0b565561d947bd9fc8d20c461
htetaunghlyan13@gmail.com <htetaunghlyan13@gmail.com>
2025-02-23 19:08:56 +0630

    Enhance datetimeEG.py to include target date comparison and updated formatting

--- datetimeEG.py
+++ datetimeEG.py
@@ -5,6 +5,11 @@ today=datetime.date.today()
 time=datetime.time(12,45,30)
 now=datetime.datetime.now()
 
-now = now.strftime("%H:%M:%S")
-
-print(now)
+now = now.strftime("%H:%M:%S %m-%d-%Y")
+target_datetime=datetime.datetime(2030,1,3,12,45,33)
+current_datetime=datetime.datetime.now()
+if target_datetime<current_datetime:
+    print("The Target Date has passed.")
+else:
+    print("The Target Date has not passed yet.")
+# print(now) 
